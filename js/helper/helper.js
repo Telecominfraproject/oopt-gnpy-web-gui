c18fdd0ce33a6fb08021306941483a9471c98a01
sakv2e10204 <sakthisivavelan.n@veetechnologies.com>
2022-05-31 16:30:14 +0530

    Added rename and removed files

---
+++ js/helper/helper.js
@@ -0,0 +1,31 @@
+﻿var nodeType = {
+    ROADM: 1,
+    Attenuator: 3,
+    Transceiver: 4,
+    ILA: 2,
+    Amplifier: 5,
+    RamanAmplifier: 6
+}
+var fiberType = {
+    SingleFiber: 1,
+    DualFiber: 2,
+    SinglePatch: 3,
+    DualPatch: 4,
+}
+
+var topologyView = {
+    NE_View: 1,
+    Functional_View: 2
+}
+
+var edgeType = {
+    fiber: 1,
+    patch: 2,
+    service: 3
+}
+var alertType = {
+    Success: 1,
+    Info: 2,
+    Error: 3,
+    Warning: 4,
+}
